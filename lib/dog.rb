d813068b8b746595a23bacf5ec442b74ba8eaf5d
Susana <susana.m.vik@gmail.com>
2021-02-22 22:22:23 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    def intialize(name)
+        @name=name
+    end
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def intialize(breed)
+        @breed=breed
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
+end 
+
